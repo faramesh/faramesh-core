3174ffe2916eac28b68b471ce9a174fbe166fb00
amjad-fatmi <mjaad7z@proton.me>
2026-04-18 19:42:30 -0400

    cmd: wire lifecycle/approvals path and broker readiness

--- cmd/faramesh/main.go
+++ cmd/faramesh/main.go
@@ -24,6 +24,7 @@ func main() {
 	if commit != "" {
 		rootCmd.Version = fmt.Sprintf("%s (%s)", version, commit)
 	}
+	configureCommandSurface()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
